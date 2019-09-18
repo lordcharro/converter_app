fb23dc98622d817262861f86e528d05d6f4c38d3
Ivo Lopes <ilopes@openwt.com>
2019-09-18 15:20:08 +0200

    Update routes to be stateful

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -2,18 +2,25 @@ import 'package:converter_app/unit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ConverterRoute extends StatelessWidget {
+class ConverterRoute extends StatefulWidget {
   final List<Unit> units;
   final Color color;
 
-  const ConverterRoute({@required this.units, @required this.color
-  }) : assert(units != null), assert(color != null);
+  @override
+  _ConverterRoute createState() => _ConverterRoute();
 
+  const ConverterRoute({@required this.units, @required this.color})
+      : assert(units != null),
+        assert(color != null);
+}
+
+class _ConverterRoute extends State<ConverterRoute>
+{
   @override
   Widget build(BuildContext context) {
-    final unitWidgets = units.map((Unit unit) {
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
-        color: color,
+        color: widget.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
