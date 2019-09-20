ddb821a9860d5dc2f931a424e0bded59632e95ca
Ivo Lopes <ilopes@openwt.com>
2019-09-20 10:31:25 +0200

    Add json categories

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -1,11 +1,11 @@
+import 'dart:convert';
+
 import 'package:converter_app/backdrop.dart';
 import 'package:converter_app/category.dart';
 import 'package:converter_app/category_tile.dart';
 import 'package:converter_app/unit.dart';
 import 'package:converter_app/unit_converter.dart';
 import 'package:flutter/material.dart';
-
-final _backgroundColor = Colors.green[100];
 
 /// Category Route (screen).
 /// This is the 'home' screen of the Unit Converter. It shows a header and
@@ -24,16 +24,6 @@ class _CategoryRouteState extends State<CategoryRoute> {
   Category _defaultCategory;
   Category _currentCategory;
   final _categories = <Category>[];
-  static const _categoryNames = <String>[
-    'Length',
-    'Area',
-    'Volume',
-    'Mass',
-    'Time',
-    'Digital Storage',
-    'Energy',
-    'Currency',
-  ];
 
   static const _baseColors = <ColorSwatch>[
     ColorSwatch(0xFF6AB7A8, {
@@ -77,25 +67,46 @@ class _CategoryRouteState extends State<CategoryRoute> {
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    for (var i = 0; i < _categoryNames.length; i++) {
+  Future<void> _retrieveLocalCategories() async{
+
+    final json = DefaultAssetBundle.of(context).loadString('assets/data/regular_units.json');
+    final data = JsonDecoder().convert(await json);
+
+    if(data is! Map){
+      throw('Data retrived from API is not a Map');
+    }
+    var categoryIndex = 0;
+    data.keys.forEach((key) {
+      final List<Unit> units = data[key].map<Unit>((dynamic data) => Unit.fromJson(data)).toList();
+
       var category = Category(
-        name: _categoryNames[i],
-        color: _baseColors[i],
+        name: key,
+        units: units,
+        color: _baseColors[categoryIndex],
         iconLocation: Icons.cake,
-        units: _retrieveUnitList(_categoryNames[i]),
       );
-      if (i == 0) {
-        _defaultCategory = category;
-      }
-      _categories.add(category);
+      setState(() {
+        if (categoryIndex == 0) {
+          _defaultCategory = category;
+        }
+        _categories.add(category);
+      });
+      categoryIndex += 1;
+    });
+  }
+
+  // wait for our JSON asset to be loaded in (async).
+  @override
+  Future<void> didChangeDependencies() async {
+    super.didChangeDependencies();
+    // We have static unit conversions located in our
+    // assets/data/regular_units.json
+    if (_categories.isEmpty) {
+      await _retrieveLocalCategories();
     }
   }
 
-  Widget _buildCategoryWidgets()
-  {
+  Widget _buildCategoryWidgets() {
     if (MediaQuery.of(context).orientation == Orientation.portrait) {
       return ListView.builder(
         itemBuilder: (BuildContext context, int index) {
@@ -118,31 +129,22 @@ class _CategoryRouteState extends State<CategoryRoute> {
         }).toList(),
       );
     }
-}
+  }
 
-/// Returns a list of mock [Unit]s.
-List<Unit> _retrieveUnitList(String categoryName) {
-  return List.generate(10, (int i) {
-    i += 1;
-    return Unit(
-      name: '$categoryName Unit $i',
-      conversion: i.toDouble(),
+
+  @override
+  Widget build(BuildContext context) {
+    final listView = Padding(
+      padding: EdgeInsets.only(left: 8.0, right: 8.0, bottom: 48.0),
+      child: _buildCategoryWidgets(),
     );
-  });
+
+    return Backdrop(
+      currentCategory: _currentCategory == null ? _defaultCategory : _currentCategory,
+      frontPanel: _currentCategory == null ? UnitConverter(category: _defaultCategory) : UnitConverter(category: _currentCategory),
+      backPanel: listView,
+      frontTitle: Text('Unit Converter'),
+      backTitle: Text('Select a Category'),
+    );
+  }
 }
-
-@override
-Widget build(BuildContext context) {
-  final listView = Padding(
-    padding: EdgeInsets.only(left: 8.0, right: 8.0, bottom: 48.0),
-    child: _buildCategoryWidgets(),
-  );
-
-  return Backdrop(
-    currentCategory: _currentCategory == null ? _defaultCategory : _currentCategory,
-    frontPanel: _currentCategory == null ? UnitConverter(category: _defaultCategory) : UnitConverter(category: _currentCategory),
-    backPanel: listView,
-    frontTitle: Text('Unit Converter'),
-    backTitle: Text('Select a Category'),
-  );
-}}
